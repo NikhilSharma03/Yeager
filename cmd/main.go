1d044e3f4db7aa91742841378ab2cbe7174644ba
NikhilSharma03 <nikhilsharmamusic2000@gmail.com>
2023-01-07 18:04:55 +0530

    feat: connect to postgres DB

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/NikhilSharma03/Yeager/config"
+	"github.com/NikhilSharma03/Yeager/internal/repository"
 )
 
 func main() {
 	// Load environment variables
 	cfg, err := config.LoadConfig(".")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("failed to load environment variables!", err)
 	}
-	fmt.Println(cfg)
+
+	// Initialize logger
+	logger := log.New(os.Stdout, "yeager", log.LstdFlags)
+
+	// Connect postgres DB
+	dbClient, err := repository.ConnectDB(&cfg)
+	if err != nil {
+		log.Fatal("failed to connect to DB!", err)
+	}
+
+	// Initialize DAO
+	dao := repository.NewDAO(dbClient)
+	fmt.Println(dao, logger)
+
 }
---
+++ internal/repository/dao.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/NikhilSharma03/Yeager/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// The DB variable will contain the database client
+// It is initialized when a new DAO is create
+var DB *gorm.DB
+
+// DAO interface
+type DAO interface{}
+
+// The dao struct implement the DAO interface
+type dao struct{}
+
+// The NewDAO function returns a pointer to dao struct which implement DAO interface
+func NewDAO(db *gorm.DB) DAO {
+	DB = db
+	return &dao{}
+}
+
+// ConnectDB connects to postgres Database using provided config
+func ConnectDB(cfg *config.Config) (*gorm.DB, error) {
+	dbURI := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", cfg.DBHost, cfg.DBUserName, cfg.DBUserPassword, cfg.DBName, cfg.DBPort)
+	return gorm.Open(postgres.Open(dbURI), &gorm.Config{})
+}
