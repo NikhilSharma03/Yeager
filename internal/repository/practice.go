f600222f2f3e7e74c8409cd7ac18bed20280928b
NikhilSharma03 <nikhilsharmamusic2000@gmail.com>
2023-01-08 14:02:12 +0530

    feat: add GetAll in practice repository

--- internal/repository/practice.go
+++ internal/repository/practice.go
@@ -7,15 +7,24 @@ import (
 )
 
 type PracticeRepository interface {
-	Create(practice *datastruct.Practice) (*datastruct.Practice, error)
+	Create(practice *datastruct.Practice) error
+	GetAll(practices *[]datastruct.Practice) error
 }
 
 type practiceRepository struct{}
 
-func (*practiceRepository) Create(practice *datastruct.Practice) (*datastruct.Practice, error) {
+func (*practiceRepository) Create(practice *datastruct.Practice) error {
 	result := DB.Create(practice)
 	if result.Error != nil {
-		return nil, fmt.Errorf("failed to create new practice! %s", result.Error.Error())
+		return fmt.Errorf("failed to create new practice! %s", result.Error.Error())
 	}
-	return practice, nil
+	return nil
+}
+
+func (*practiceRepository) GetAll(practices *[]datastruct.Practice) error {
+	result := DB.Find(practices)
+	if result.Error != nil {
+		return fmt.Errorf("failed to fetch all practice! %s", result.Error.Error())
+	}
+	return nil
 }
