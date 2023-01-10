75963a6e839f63dde9c6627be27138a5fe96d4fd
NikhilSharma03 <nikhilsharmamusic2000@gmail.com>
2023-01-10 16:48:31 +0530

    feat: add GetByID in user repository

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,21 +1,39 @@
 package repository
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/NikhilSharma03/Yeager/internal/datastruct"
+	"gorm.io/gorm"
 )
 
 type UserRepository interface {
 	Create(user *datastruct.User) error
+	GetByID(user *datastruct.User, id string) error
 }
 
 type userRepository struct{}
+
+var (
+	errNoUserFoundByID = errors.New("found no user by provided id")
+)
 
 func (*userRepository) Create(user *datastruct.User) error {
 	result := DB.Create(user)
 	if result.Error != nil {
 		return fmt.Errorf("failed to create new user! %s", result.Error.Error())
+	}
+	return nil
+}
+
+func (*userRepository) GetByID(user *datastruct.User, id string) error {
+	result := DB.First(user, "id=?", id)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return errNoUserFoundByID
+		}
+		return fmt.Errorf("failed to fetch user by id! %s", result.Error.Error())
 	}
 	return nil
 }
